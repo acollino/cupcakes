1a7f38ffab8e48463db5fc7f835650f77c2bf554
Alex Collino <arcollino@gmail.com>
2022-07-27 16:57:06 -0700

    implement cupcake model

--- models.py
+++ models.py
@@ -1,1 +1,20 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to the cupcakes database."""
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """Class representing a cupcake with flavor, size, rating, and image."""
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default="https://tinyurl.com/demo-cupcake")
