ae91a4b9efa40662e95a49e2078019eb3bb30528
Alex Collino <arcollino@gmail.com>
2022-07-28 12:07:07 -0700

    add api routes for get and post requests

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """Flask app for Cupcakes"""
-from flask import Flask, request, render_template, redirect
+from crypt import methods
+from flask import Flask, jsonify, request, render_template, redirect
 from models import Cupcake, db, connect_db
 import os
 
@@ -14,3 +15,67 @@ app.config["SQLALCHEMY_ECHO"] = True
 app.debug = True
 
 connect_db(app)
+
+
+@app.route("/")
+def display_home():
+    pass
+
+
+# Part Two: Listing, Getting & Creating Cupcakes
+# Make routes for the following:
+
+# GET /api/cupcakes
+# Get data about all cupcakes.
+# Respond with JSON like: {cupcakes: [{id, flavor, size, rating, image}, ...]}.
+# The values should come from each cupcake instance.
+@app.route("/api/cupcakes")
+def display_all_cupcakes():
+    """Gets data about all cupcakes in the table."""
+    cupcakes_all = Cupcake.query.all()
+    cupcake_list = [cupcake.serialize() for cupcake in cupcakes_all]
+    return jsonify(cupcakes=cupcake_list)
+
+
+# GET /api/cupcakes/[cupcake-id]
+# Get data about a single cupcake.
+# Respond with JSON like: {cupcake: {id, flavor, size, rating, image}}.
+# This should raise a 404 if the cupcake cannot be found.
+@app.route("/api/cupcakes/<cupcake_id>")
+def display_single_cupcake(cupcake_id):
+    """Gets data about a single cupcake in the table."""
+    cupcake_single = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake_single.serialize())
+
+
+# POST /api/cupcakes
+# Create a cupcake with flavor, size, rating and image data from the body of the request.
+# Respond with JSON like: {cupcake: {id, flavor, size, rating, image}}.
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Create a cupcake with flavor, size, rating and image data from the request."""
+    cupcake_new = Cupcake(**request.json)
+    db.session.add(cupcake_new)
+    db.session.commit()
+    return (jsonify(cupcake=cupcake_new.serialize()), 201)
+
+
+# Part Three: Update & Delete Cupcakes
+# Make routes for the following:
+
+# PATCH /api/cupcakes/[cupcake-id]
+# Update a cupcake with the id passed in the URL and flavor, size, rating and image data from the body of the request. You can always assume that the entire cupcake object will be passed to the backend.
+
+# This should raise a 404 if the cupcake cannot be found.
+
+# Respond with JSON of the newly-updated cupcake, like this: {cupcake: {id, flavor, size, rating, image}}.
+
+# DELETE /api/cupcakes/[cupcake-id]
+# This should raise a 404 if the cupcake cannot be found.
+
+# Delete cupcake with the id passed in the URL. Respond with JSON like {message: "Deleted"}.
+
+# Test these routes in Insomnia.
+
+# Part Four: Write More Tests
+# Add tests for the PATCH and DELETE routes.
